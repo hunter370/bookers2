198aa6c3999d20858ca779941841033032618c69
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-03-13 05:29:55 +0000

    [add] flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+     flash[:notice] = "You have created book successfully."
      redirect_to books_path
     else
       render :new
@@ -23,6 +24,16 @@ class BooksController < ApplicationController
   
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
