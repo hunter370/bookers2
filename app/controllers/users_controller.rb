bfdced6d462ee53ea210bddd3f98f8c4bb70fa06
EC2 Default User <ec2-user@ip-172-31-16-150.ap-northeast-1.compute.internal>
2023-03-14 06:01:29 +0000

    [add] flash new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
   
+  def create
+    @user = User.new(user_params)
+    @user.save
+    flash[:notice] = "Welcome! You have signed up successfully."
+    redirect_to user_path
+  end
+  
   def index
     @users = User.all
   end
